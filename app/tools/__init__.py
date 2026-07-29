f3c7395f583d7a0e83775892cd0144696d717304
vansh-gautam-bit <vansh.pku@gmail.com>
2026-07-30 00:09:05 +0530

    made owner_tools to use

---
+++ app/tools/__init__.py
@@ -0,0 +1,22 @@
+from app.tools.owner_tools import *
+from app.tools.pet_tools import *
+from app.tools.visit_tools import *
+
+TOOLS = [
+    get_all_owners,
+    get_owner_by_id,
+    find_owner_by_name,
+    create_owner,
+    update_owner,
+    delete_owner,
+
+    create_pet,
+    get_pet,
+    update_pet,
+    delete_pet,
+
+    create_visit,
+    get_visit,
+    update_visit,
+    delete_visit,
+]
---
+++ app/tools/owner_tools.py
@@ -0,0 +1,80 @@
+from langchain_core.tools import tool
+from app.services.api_client import api_client
+
+@tool 
+def get_all_owner():
+    """
+    Retrieve all owners from the Vetrinary Management System.
+    """
+    return api_client.get("/owners")
+
+@tool
+def get_owner_by_id(owner_id: int):
+    """
+    Retrieve an owner using their unique ID.
+    """
+    return api_client.get(f"/owners/{owner_id}")
+
+@tool
+def find_owner_by_name(name: str):
+    """
+    Find an owner by name.
+    Returns the matching owner if found.
+    """
+
+    owners = api_client.get("/owners")
+
+    for owner in owners:
+        if owner["name"].strip().lower() == name.strip().lower():
+            return owner
+
+    return {
+        "message": f"No owner found with name '{name}'."
+    }    
+
+@tool
+def create_owner(
+    name: str,
+    phone: str,
+    email: str,
+):
+    """
+    Create a new owner.
+    """ 
+
+    return api_client.post(
+        "/owners/",
+        json={
+            "name": name,
+            "phone": phone,
+            "email": email,
+        },
+    )
+
+@tool
+def update_owner(
+    owner_id: int,
+    name: str,
+    phone: str,
+    email: str,
+):
+    """
+    Update an existing owner.
+    """
+
+    return api_client.put(
+        f"/owners/{owner_id}",
+        json={
+            "name": name,
+            "phone": phone,
+            "email": email,
+        },
+    )
+
+@tool
+def delete_owner(owner_id: int):
+    """
+    delete an owner.
+    """
+
+    return api_client.delete(f"/owners/{owner_id}")
